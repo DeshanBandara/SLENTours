899a071bf4e3816ae6876bc7d698e1580cf3a31e
DeshanBandara <sudheeradeshan1013@gmail.com>
2025-01-01 01:50:09 +0530

    Imported screens into main index.js file

--- SLENTours/app/index.js
+++ SLENTours/app/index.js
@@ -1,11 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import homeScreen from './screens/homeScreen'
+import indexScreen from './screens/indexScreen'
+import loginScreen from './screens/loginScreen'
+import signinScreen from './screens/signinScreen'
+
+const Stack = createNativeStackNavigator();
 
 const index = () => {
   return (
-    <View>
-      <Text>Hellow World</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen
+        name='indexScreen'
+        component={indexScreen}
+        options={{title: 'Welcome!'}}
+      />
+      
+    </Stack.Navigator>
   )
 }
 
