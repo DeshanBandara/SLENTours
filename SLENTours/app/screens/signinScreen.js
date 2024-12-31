4fc91549a26eb170ef23dea634ee4817394cdf2d
DeshanBandara <sudheeradeshan1013@gmail.com>
2025-01-01 01:29:40 +0530

    Added react basics

---
+++ SLENTours/app/screens/signinScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const signinScreen = () => {
+  return (
+    <View>
+      <Text>signinScreen</Text>
+    </View>
+  )
+}
+
+export default signinScreen
+
+const styles = StyleSheet.create({})
